7d47dd89df14d0b6e64208c9df95812492497c13
Luis Eduardo Bueso de Barrio <luiseduardobueso96@gmail.com>
2020-07-19 12:07:27 +0200

    Initial commit

---
+++ init.el
@@ -0,0 +1,19 @@
+(require 'org)
+
+(org-babel-load-file
+ (expand-file-name "conf/general.org" user-emacs-directory))
+
+(org-babel-load-file
+ (expand-file-name "conf/theme.org" user-emacs-directory))
+
+(org-babel-load-file
+ (expand-file-name "conf/editing.org" user-emacs-directory))
+
+(org-babel-load-file
+ (expand-file-name "conf/tools.org" user-emacs-directory))
+
+(org-babel-load-file
+ (expand-file-name "conf/programming.org" user-emacs-directory))
+
+(org-babel-load-file
+ (expand-file-name "conf/mail/mu4e.org"))
